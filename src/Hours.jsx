e106968c07a12092e7b2d5b728bc2ce4e8eb32d3
Angel Fabrizio Franyutti Pulido <afabri24@gmail.com>
2024-04-09 14:09:07 -0600

    Recuperacion y put

--- src/Hours.jsx
+++ src/Hours.jsx
@@ -1,50 +1,121 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import dayjs from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import Time from "./TimerPickerComponent.jsx";
 import { MobileTimePicker } from "@mui/x-date-pickers/MobileTimePicker";
 import { Switch } from "@mui/material";
-import { Select, MenuItem } from '@mui/material';
+import { Select, MenuItem } from "@mui/material";
 import axios from "axios";
+import API_URL from "./utils/Constantes";
+import Cookies from "universal-cookie";
 
-function Hours({idDiaHora, horaInicio, horaFin,modalidad, selectedDate, handleDateChange }) {
-  const [isChecked, setIsChecked] = useState(false);
+function Hours({
+  idDiaHora,
+  horaInicio,
+  horaFin,
+  modalidad,
+  dia,
+  esLibre,
+  estado,
+  selectedDate,
+  handleDateChange,
+  handleReload,
+}) {
+  const cookies = new Cookies();
+  const [mode, setMode] = useState(modalidad);
+  const [isChecked, setIsChecked] = useState(true)
+  const [status, setStatus] = useState(estado);
+  const [bloqueado, setBloqueado] = useState(status);
+  const [modo,setModo]=useState(mode);
 
+  
   const handleSwitchChange = (event) => {
+    if(event.target.checked){
+      setStatus("activo");
+      setBloqueado("inactivo");
+    }else{
+      setStatus("inactivo");
+      setBloqueado("activo");
+    } 
+    console.log(status);
+    console.log(bloqueado);
     setIsChecked(event.target.checked);
+    updateData();
   };
 
-
   const handleModeChange = (event) => {
+    if(event.target.value==="virtual"){
+      setModo("presencial");
+    }else{
+      setModo("virtual");
+    }
+    console.log(mode);
+    console.log(modo);
     setMode(event.target.value);
+    updateData();
+    
   };
 
   const handleDelete = async () => {
+    const token = localStorage.getItem("token");
     if (window.confirm("¿Estás seguro de que quieres eliminar este horario?")) {
       try {
-        const response = await axios.post(
-          API_URL + "api/asesores/eliminarDiaHora/",
-          {
+        const response = await axios({
+          method: "DELETE",
+          url: API_URL + "api/asesores/eliminarDiaHora/",
+          data: {
             id_diahora: idDiaHora,
-          }
-        );
-  
-  
-        if(!response.data.error){
-          alert("Se agrego el nuevo horario");
-          fetchHorarios();
-        }else{
-          alert("error al agregar el horario")
-          console.error(error);
+            token: token,
+          },
+        });
+        if (!response.data.error) {
+          alert("Horario eliminado");
+          handleReload();
+        } else {
+          alert("Hubo un error al eliminar el horario");
         }
       } catch (error) {
-        console.error(error);
+        console.error("Error al eliminar el horario", error);
       }
     }
   };
 
+  const updateData = async () => {
+    console.log(idDiaHora);
+    console.log(dia);
+    console.log(horaInicio);
+    console.log(horaFin);
+    console.log(modo);
+    console.log(bloqueado);
+    const token = localStorage.getItem("token");
+    try {
+      const response = await axios({
+        method: "PUT",
+        url: API_URL + "api/asesores/actualizarDiaHora/",
+        data: {
+          idDiaHora: idDiaHora,
+          dia: dia.toString().toLowerCase(),
+          hora_inicio: horaInicio,
+          hora_termino: horaFin,
+          modalidad: modo,
+          esLibre: esLibre,
+          estado: bloqueado,
+          token: token,
+        },
+      });
 
+      console.log(response.data);
+      if (!response.data.error) {
+        alert("Datos actualizados");
+        handleReload();
+      } else {
+        alert("Hubo un error al actualizar los datos");
+      }
+    } catch (error) {
+      console.error("Error al actualizar los datos", error);
+    }
+  };
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -52,32 +123,33 @@ function Hours({idDiaHora, horaInicio, horaFin,modalidad, selectedDate, handleDa
         value={selectedDate}
         defaultValue={dayjs(horaInicio, "HH:mm")}
         onChange={handleDateChange}
+        // onAccept={handleDateChangeStart}
         renderInput={(params) => <Time {...params} />}
-        disabled={!isChecked}
       />
       <MobileTimePicker
         value={selectedDate}
         defaultValue={dayjs(horaFin, "HH:mm")}
         onChange={handleDateChange}
+        // onAccept={handleDateChangeEnd}
         renderInput={(params) => <Time {...params} />}
-        disabled={!isChecked}
       />
 
       {/* switch desactivacion hora */}
-      <div className="content-center">Editar?</div>
-      <Switch checked={isChecked} onChange={handleSwitchChange} />
+      <Switch checked={status==="activo"} onChange={handleSwitchChange} />
 
       {/* cambiar a virtal o presencial */}
 
-      <Select value={modalidad} onChange={handleModeChange}>
+      <Select value={mode} onChange={handleModeChange}>
         <MenuItem value={"presencial"}>Presencial</MenuItem>
         <MenuItem value={"virtual"}>Virtual</MenuItem>
       </Select>
 
       {/* eliminar horario de ese dia */}
 
-      <button className="mt-3 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-      onClick={handleDelete}>
+      <button
+        className="mt-3 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleDelete}
+      >
         Eliminar
       </button>
     </LocalizationProvider>
