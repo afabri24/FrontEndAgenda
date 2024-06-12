a330193df7697ddb098498d9b2feeabb3cc65ade
Diego178 <89479225+Diego178@users.noreply.github.com>
2024-06-12 11:49:21 -0600

    commit

--- src/Admin/Asesores/ModalDetalles.jsx
+++ src/Admin/Asesores/ModalDetalles.jsx
@@ -1,37 +1,46 @@
 import React, { useContext, useEffect } from 'react'
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import { Modal, Button } from "flowbite-react";
 import { dataContext } from '../ContextoAdmin';
 
-function ModalDetalles({open, handleClose}) {
-    // const { asesor } = useContext(dataContext)
-
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-      };
-
-      // useEffect(() =>{
-      //   console.log(asesor)
-      // })
+function ModalDetalles({open, setOpen}) {
+    
+    const { asesor } = useContext(dataContext)
+    useEffect(() =>{
+      
+      console.log(asesor)
+    })
 
   return (
-    <Modal
-    open={open}
-    onClose={handleClose}
-    aria-labelledby="modal-modal-title"
-    aria-describedby="modal-modal-description">
-   
-
-    </Modal>
+    <Modal show={open} onClose={() => setOpen(false)}>
+        {asesor && <><Modal.Header>Datos de {asesor.nombre}</Modal.Header>
+        <Modal.Body>
+          <div className="grid grid-cols-2">
+            <div>
+              <h2 className="text-base leading-relaxed text-gray-500 ">
+                Correo Electronico:
+              </h2>
+              <p className="text-base leading-relaxed text-gray-700 ">
+                {asesor.email}
+              </p>
+              <h2 className="text-base leading-relaxed text-gray-500 ">
+                Idioma:
+              </h2>
+              <p className="text-base leading-relaxed text-gray-700 ">
+                {asesor.idioma}
+              </p>
+            </div>
+            <div className='max-w-40 max-y-40'>
+              <img src={asesor.fotoBase64} className='border-2 rounded-lg' />
+            </div>
+          </div>
+        </Modal.Body>
+        </>}
+        <Modal.Footer>
+          <Button onClick={() => setOpen(false)}>Aceptar</Button>
+        </Modal.Footer> 
+      </Modal>
   )
 }
 
